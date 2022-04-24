660e911e6a3192d6cce5c36fd946cb65abd8a000
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-04-24 15:56:09 +0600

    registration system implimented with react-firebase-hooks

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,30 +1,61 @@
-import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const getEmail = event.target.email.value;
+    const getPassword = event.target.password.value;
+    setEmail(getEmail);
+    setPassword(getPassword);
+    console.log(user);
+  };
+  let successMessegeElement;
+  if (user) {
+    successMessegeElement = (
+      <div>
+        <p>Signed In User: {user.email}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container my-5 w-50">
       <h1>Please Register</h1>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Form.Group className="mb-3">
           <Form.Control type="text" placeholder="Your Name" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control name="email" type="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button
+          onClick={() => createUserWithEmailAndPassword(email, password)}
+          variant="primary"
+          type="submit"
+        >
           Submit
         </Button>
+        {successMessegeElement};
       </Form>
       <div className="my-3">
-        <span>
-          Already have an account?
-        </span>
+        <span>Already have an account?</span>
         <span className="mx-1">
           <Link
             style={{
